3930f337cc183624fef92a4fa99d2e26e62599ed
albuska <109683293+albuska@users.noreply.github.com>
2023-01-20 22:05:53 +0200

    finish 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const containerRef = document.querySelector('.gallery');
+
+const createGalleryList = createGalleryItem(galleryItems); 
+containerRef.innerHTML = createGalleryList; 
+
+function createGalleryItem(items) {
+return items.map(item => 
+    `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      title="${item.description}"
+    />
+    </a>`
+    ).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {'captionDelay': 250});
+    // lightbox =  $('.gallery a').simpleLightbox({'captionDelay': 250});
+
+
+
+
+
+   
+
+
+
+
+
+
+
